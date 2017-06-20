66461a39c9e4a4b40c755d97a007c467a6236665
John Mark Perocho <per.eight@gmail.com>
2017-06-20 14:09:40 +0800

    Added Command

--- Gemfile
+++ Gemfile
@@ -2,3 +2,11 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in i.gemspec
 gemspec
+
+group :development do
+  gem "vagrant", :git => "git://github.com/mitchellh/vagrant.git"
+end
+
+group :plugins do 
+  gem "i", path: "."
+end
--- lib/i.rb
+++ lib/i.rb
@@ -1,5 +1,12 @@
 require "i/version"
 
 module I
-  # Your code goes here...
+  class Plugin < Vagrant.plugin("2")
+    name "I development and deployment automation plugin"
+  
+    command "free-memory" do
+      require_relative "i/command"
+      Command
+    end
+  end
 end
